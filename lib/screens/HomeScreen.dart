62e3ac9d520789991faeee2d2a1d16bf83c979db
Maya <melokitty.1003@gmail.com>
2021-04-21 20:31:06 +0900

    add button

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -12,10 +12,37 @@ class HomeScreen extends StatelessWidget {
         automaticallyImplyLeading: false,
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            child: Text('Hello'),
-          ),
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: ElevatedButton(
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 300,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Colors.orange[300],
+                        Colors.orange[500],
+                        Colors.orange[700]
+                      ]
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: const Text('Gradient Button', style: TextStyle(color: Colors.white)),
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.all(0)
+                ),
+              )
+              
+            ),
+          )
         ],
       ),
     );
