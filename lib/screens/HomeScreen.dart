1e35d36bada053c83df73fd9a9f94b4feb954fb4
Maya <melokitty.1003@gmail.com>
2021-04-21 23:17:49 +0900

    made component for selectbuton

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -18,33 +18,100 @@ class HomeScreen extends StatelessWidget {
           Container(
             alignment: Alignment.center,
             child: Padding(
+                padding: EdgeInsets.all(20),
+                child: SelectButton(
+                  buttonText: '給与データ登録',
+                  lightColor: Colors.yellow[300],
+                  middleColor: Colors.yellow[500],
+                  darkColor: Colors.yellow[700],
+                  onPress: () => print('good bye'),
+                )
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Padding(
               padding: EdgeInsets.all(20),
-              child: ElevatedButton(
-                child: Container(
-                  alignment: Alignment.center,
-                  width: 300,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: <Color>[
-                        Colors.orange[300],
-                        Colors.orange[500],
-                        Colors.orange[700]
-                      ]
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: const Text('Gradient Button', style: TextStyle(color: Colors.white)),
-                ),
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.all(0)
-                ),
+              child: SelectButton(
+                buttonText: '給与データ編集',
+                lightColor: Colors.orange[300],
+                middleColor: Colors.orange[500],
+                darkColor: Colors.orange[700],
+                onPress: () => print('good bye'),
               )
-              
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Padding(
+                padding: EdgeInsets.all(20),
+                child: SelectButton(
+                  buttonText: '給与データを見る',
+                  lightColor: Colors.pink[300],
+                  middleColor: Colors.pink[500],
+                  darkColor: Colors.pink[700],
+                  onPress: () => print('good bye'),
+                )
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: Padding(
+                padding: EdgeInsets.all(20),
+                child: SelectButton(
+                  buttonText: 'メンバー編集',
+                  lightColor: Colors.red[300],
+                  middleColor: Colors.red[500],
+                  darkColor: Colors.red[700],
+                  onPress: () => print('good bye'),
+                )
             ),
           )
         ],
       ),
+    );
+  }
+}
+
+class SelectButton extends StatelessWidget {
+
+  @required String buttonText;
+  @required Color lightColor;
+  @required Color middleColor;
+  @required Color darkColor;
+  @required Function onPress;
+
+  SelectButton({
+    this.buttonText,
+    this.lightColor,
+    this.middleColor,
+    this.darkColor,
+    this.onPress
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Container(
+        alignment: Alignment.center,
+        width: 300,
+        height: 50,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: <Color>[
+              lightColor,
+              middleColor,
+              darkColor
+            ]
+          ),
+        ),
+        padding: const EdgeInsets.all(10),
+        child: Text(buttonText, style: TextStyle(color: Colors.white, fontSize: 20)),
+      ),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(0)
+      ),
+      onPressed: onPress,
     );
   }
 }
