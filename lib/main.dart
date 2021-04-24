797754fd6cc285506b9807a78059aab232fff203
Maya <melokitty.1003@gmail.com>
2021-04-24 18:13:20 +0900

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:connect_project/screens/DeleteMemberScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/HomeScreen.dart';
 import 'screens/AddMemberScreen.dart';
 import 'screens/EditMemberScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
