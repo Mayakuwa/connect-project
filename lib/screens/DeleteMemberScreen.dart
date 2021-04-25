78ca1f96efbcab089380d77c65839d3e802d0486
Maya <melokitty.1003@gmail.com>
2021-04-25 21:45:50 +0900

    fix delete function

--- lib/screens/DeleteMemberScreen.dart
+++ lib/screens/DeleteMemberScreen.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 class DeleteMemberScreen extends StatelessWidget {
 
-  final _firestore = FirebaseFirestore.instance;
   static const routeName = './delete_member_screen';
+
 
   @override
   Widget build(BuildContext context) {
+
+    Future<void> showCheckDeleteDialog(DocumentReference documentId) async {
+      await showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: Text("削除"),
+              content: Text("本当に削除しますか？"),
+              actions: <Widget>[
+                // ボタン領域
+                FlatButton(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                FlatButton(
+                  child: Text("OK"),
+                  onPressed: () {
+                    documentId.delete();
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            );
+          }
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('メンバー削除'),
@@ -33,8 +63,8 @@ class DeleteMemberScreen extends StatelessWidget {
                             trailing: IconButton(
                                 icon: Icon(Icons.delete),
                                 onPressed: () async {
-                                  await FirebaseFirestore.instance
-                                      .collection('members').doc(document.id).delete();
+                                  await showCheckDeleteDialog(FirebaseFirestore.instance
+                                      .collection('members').doc(document.id));
                                },
                             ),
                           ),
