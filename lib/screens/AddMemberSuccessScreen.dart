767afcbdd2c0abdd7f730e260274ec9ce38aec79
Maya <melokitty.1003@gmail.com>
2021-04-26 21:46:12 +0900

    add animation

--- lib/screens/AddMemberSuccessScreen.dart
+++ lib/screens/AddMemberSuccessScreen.dart
@@ -1,8 +1,10 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 
 class AddMemberSuccessScreen extends StatelessWidget {
   static const routeName = './add_member_success_member';
 
+  //戻るボタンをつけないよにする。
   @override
   Widget build(BuildContext context) {
     final mamaName = ModalRoute.of(context).settings.arguments;
@@ -10,7 +12,20 @@ class AddMemberSuccessScreen extends StatelessWidget {
       appBar: AppBar(title: Text('メンバー追加02')),
       body: Column(
         children: [
-          Text(mamaName)
+          SizedBox(
+            height: 100,
+            child: FlareActor(
+                'images/SuccessCheck.flr',
+                alignment: Alignment.center,
+                animation: 'Untitled',
+                fit: BoxFit.contain
+            ),
+          ),
+          Text(
+            '$mamaNameが\n追加されました😍',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+          )
         ],
       )
     );
